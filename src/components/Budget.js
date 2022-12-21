401e15c4f27915cd5774a50e33760853f473ab92
sapthashree15 <kssapthashree@gmail.com>
2022-12-21 06:37:19 -0500

    changes made from theia lab environment

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,13 +2,30 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+	const { budget,dispatch,expenses,Currency } = useContext(AppContext);
 
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+	const changeBudget = (val)=>{
+		const totalExpenses = expenses.reduce((total, item) => {
+			return (total += item.cost);
+		}, 0);
+
+		
+		if(val<totalExpenses) {
+			alert("You cannot reduce the budget that is already allocated!");
+		} else {
+			dispatch({
+				type: 'SET_BUDGET',
+				payload: val,
+			})
+			}
+	}
+	
+	return (
+		<div className='alert alert-secondary'>
+        <span>Budget: {Currency} </span>
+			<input type="number" step="10" max="20000" value={budget} onInput={(event)=>changeBudget(event.target.value)}></input>
+		</div>
+	);
 };
 
 export default Budget;
--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -2,17 +2,17 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseTotal = () => {
-    const { expenses } = useContext(AppContext);
+	const { expenses, currency } = useContext(AppContext);
 
-    const totalExpenses = expenses.reduce((total, item) => {
-        return (total += item.cost);
-    }, 0);
+	const totalExpenses = expenses.reduce((total, item) => {
+		return (total += item.cost);
+	}, 0);
 
-    return (
-        <div className='alert alert-primary'>
-            <span>Spent so far: £{totalExpenses}</span>
-        </div>
-    );
+	return (
+		<div className='alert alert-primary'>
+			<span>Spent so far: {currency}{totalExpenses}</span>
+		</div>
+	);
 };
 
 export default ExpenseTotal;
